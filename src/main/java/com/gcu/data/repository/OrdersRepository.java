ca4aa830cdb181b896b04c4abf646f398db41e76
riiansmart <rismart1221@outlook.com>
2025-01-29 10:07:00 -0700

    changed uppercase ORDERS to orders for sql table

--- src/main/java/com/gcu/data/repository/OrdersRepository.java
+++ src/main/java/com/gcu/data/repository/OrdersRepository.java
@@ -1,18 +1,18 @@
 package com.gcu.data.repository;
 
+import java.util.List;
+
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-
-import com.gcu.data.entity.OrderEntity;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.gcu.data.entity.OrderEntity;
 
 @Repository
 public interface OrdersRepository extends CrudRepository<OrderEntity, Long> {
     //example of truly overriding a method from the crudRepository and using our own customized sql
     @Override
-    @Query(value = "SELECT * FROM ORDERS")
+    @Query(value = "SELECT * FROM orders")
     public List<OrderEntity> findAll();
 
 }
