8b53b13d1a5a3d77f11562d3b0885612eacf1be6
Michael Smart <rismart1221@outlook.com>
2025-02-04 12:48:09 -0700

    commented out mapping request in login controller

--- src/main/java/com/gcu/controller/LoginController.java
+++ src/main/java/com/gcu/controller/LoginController.java
@@ -10,12 +10,13 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/login")
+// Commenting out Request Mapping for '/login'
+// @RequestMapping("/login")
 public class LoginController {
 
     private OrdersBusinessServiceInterface ordersService;
